54b869efd82ac41cd10430c92bc9bbcb7c616858
jessamynsmith <jessamyn.smith@gmail.com>
2020-04-10 20:38:59 -0600

    Refactored api endpoints to share code. Put api endpoints on api url path.

--- index.js
+++ index.js
@@ -4,29 +4,31 @@ const csv = require('csvtojson')
 const PORT = process.env.PORT || 5000
 
 
-const fish = async (req, res) => {
-    console.log("fish")
-
-    const csvFilePath = "data/AnimalCrossingCritternariumFish.csv";
+const loadCsvAsJson = async (csvFilePath) => {
     const jsonArray = await csv().fromFile(csvFilePath);
+    return jsonArray;
+};
+
+
+const fish = async (req, res) => {
+    const csvFilePath = "data/AnimalCrossingCritternariumFish.csv";
+    const jsonArray = await loadCsvAsJson(csvFilePath);
 
     res.json(jsonArray);
-}
+};
 
 const bugs = async (req, res) => {
-    console.log("bugs")
-
     const csvFilePath = "data/AnimalCrossingCritternariumBugs.csv";
-    const jsonArray = await csv().fromFile(csvFilePath);
+    const jsonArray = await loadCsvAsJson(csvFilePath);
 
     res.json(jsonArray);
-}
+};
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-    .get('/fish', fish)
-    .get('/bugs', bugs)
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .get('/api/v1/fish', fish)
+    .get('/api/v1/bugs', bugs)
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
